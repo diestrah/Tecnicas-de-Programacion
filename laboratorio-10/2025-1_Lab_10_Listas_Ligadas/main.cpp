1bf6029b27dd406c11e752b350c486ac50e5704a
diestrah <n.diestrah@pucp.edu.pe>
2026-08-23 17:24:17 -0500

    Update main.cpp

--- laboratorio-10/2025-1_Lab_10_Listas_Ligadas/main.cpp
+++ laboratorio-10/2025-1_Lab_10_Listas_Ligadas/main.cpp
@@ -1,13 +1,12 @@
 /*---------------------------------------------------------------------------------------*/
+/*  Elaborado por       :   Naim Diestra
 /*  Fecha de creación   :   17/06/2026
-/*  Autor               :   Naim Diestra
-/*  Descripción del problema:
-/*  Se leen los archivos "CuentasBancarias.csv" y "MovimientosDeCuentas.csv" y se llena
-/*  la data en el Nodo* Lista. Se emite un reporte inicial de todas las cuentas y
-/*  después se eliminan todas las cuentas cuyo saldo sea mayor a 0. Finalmente se emite
-/*  un reporte final donde se muestran las cuentas a eliminar.
+/*  Descripción         :   Se leen los archivos "CuentasBancarias.csv" y 
+/*  "MovimientosDeCuentas.csv" y se llena la data en el Nodo* Lista. Se emite un reporte 
+/*  inicial de todas las cuentas y después se eliminan todas las cuentas cuyo saldo sea mayor 
+/*  a 0. Finalmente se emite un reporte final donde se muestran las cuentas a eliminar.
 /*  La finalidad del laboratorio era reforzar el concepto de lista ligada y structs
-/*  (una previa de Algoritmia)
+/*  (una previa de Algoritmia y Estructura de Datos)
 /*---------------------------------------------------------------------------------------*/
 
 #include "Bibliotecas/FuncionesAuxiliares.h"
