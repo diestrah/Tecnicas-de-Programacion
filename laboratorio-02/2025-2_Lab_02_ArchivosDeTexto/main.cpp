1dc24996ff6f15eccfcdf4c41e90d5ba6223b6f5
diestrah <n.diestrah@pucp.edu.pe>
2026-08-23 17:14:30 -0500

    Update main.cpp

--- laboratorio-02/2025-2_Lab_02_ArchivosDeTexto/main.cpp
+++ laboratorio-02/2025-2_Lab_02_ArchivosDeTexto/main.cpp
@@ -1,12 +1,12 @@
-/*
- *  Elaborado por   :   Naim Diestra
- *  Fecha           :   09/12/2023
- *  Descripción     :   Se leen los datos de twitchdataTP.txt y se emite un reporte
- *                      que emitirá los datos de los canales cuya fecha de creación
- *                      se encuentre en el rango de fechas solicitado al usuario.
- *                      Además, cada canal contará con un resumen por sus reproducciones
- *                      y se emite un resumen de todos los canales.
- */
+/*----------------------------------------------------------------------------------------*/
+/*  Elaborado por   :   Naim Diestra
+/*  Fecha creación  :   26/01/2026
+/*  Descripción     :   Se leen los datos de twitchdataTP.txt y se emite un reporte
+/*                      que emitirá los datos de los canales cuya fecha de creación
+/*                      se encuentre en el rango de fechas solicitado al usuario.
+/*                      Además, cada canal contará con un resumen por sus reproducciones
+/*                      y se emite un resumen de todos los canales.
+/*----------------------------------------------------------------------------------------*/
 
 #include "Bibliotecas/FuncionesAuxiliares.h"
 
