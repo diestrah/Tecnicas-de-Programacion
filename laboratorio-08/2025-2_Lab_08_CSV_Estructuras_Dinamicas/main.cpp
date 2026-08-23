a4b3ab1bf312b0b381699679b3d108efc60adf2f
diestrah <n.diestrah@pucp.edu.pe>
2026-08-23 17:19:52 -0500

    Update main.cpp

--- laboratorio-08/2025-2_Lab_08_CSV_Estructuras_Dinamicas/main.cpp
+++ laboratorio-08/2025-2_Lab_08_CSV_Estructuras_Dinamicas/main.cpp
@@ -1,5 +1,5 @@
 /*------------------------------------------------------------------------------------------*/
-/*  Autor       	:   Naim Diestra
+/*  Elaborado por   :   Naim Diestra
 /*  Fecha creación  :   10/12/2025
 /*  Actualización   :   03/05/2026
 /*  Descripción     :   En un array de struct Categoria se almacenan los datos de los archivos
