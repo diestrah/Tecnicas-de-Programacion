e3df8105690f4028fc34f6927f84b6a066a704f2
diestrah <n.diestrah@pucp.edu.pe>
2026-08-23 17:22:21 -0500

    Update main.cpp

--- laboratorio-09/2025-2_Lab_09_Listas_Ligadas/main.cpp
+++ laboratorio-09/2025-2_Lab_09_Listas_Ligadas/main.cpp
@@ -1,16 +1,15 @@
-/*
- *  Nombre  :   Naim Diestra
- *  Fecha   :   04/07/2026
- *
- *  Descripción:
- *  Este programa procesa la información usando una lista ligada ordenada ascendentemente por código de
- *  categoría. Primero lee las categorías desde "Categorias.csv" y genera un reporte de prueba.
- *  Luego lee las reproducciones desde "StreamsReproducidos.csv", actualiza la duración total,
- *  la duración máxima, el canal con mayor duración y los drop-off de cada categoría.
- *  Después calcula el promedio de drop-off,
- *  emite el reporte completo, elimina las categorías con promedio de drop-off menor a 3.5
- *  y finalmente genera un reporte con la lista resultante (utiliza la misma función de impresión).
- */
+/*----------------------------------------------------------------------------------------*/
+/*  Elaborado por   :   Naim Diestra
+/*  Fecha creación  :   04/07/2026
+/*  Descripción:
+/*  Este programa procesa la información usando una lista ligada ordenada ascendentemente por código de
+/*  categoría. Primero lee las categorías desde "Categorias.csv" y genera un reporte de prueba.
+/*  Luego lee las reproducciones desde "StreamsReproducidos.csv", actualiza la duración total,
+/*  la duración máxima, el canal con mayor duración y los drop-off de cada categoría.
+/*  Después calcula el promedio de drop-off,
+/*  emite el reporte completo, elimina las categorías con promedio de drop-off menor a 3.5
+/*  y finalmente genera un reporte con la lista resultante (utiliza la misma función de impresión).
+/*----------------------------------------------------------------------------------------*/
 #include "Bibliotecas/FuncionesAuxiliares.h"
 
 int main() {
